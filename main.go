948088acb4f65f2f3c1963d1fa10603b90568ca3
bloodofspring <t.shagin@gmail.com>
2024-12-01 10:39:54 +0300

    feat: Усовершенствован калькулятор

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"main/useful_things"
-	"os"
-)
+import "main/useful_things"
 
 func main() {
-	var eq string
-
-	fmt.Println("input equation:")
-	reader := bufio.NewReader(os.Stdin)
-	eq, err := reader.ReadString('\n')
-
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("%v\n", useful_things.Eval(eq))
+	useful_things.RunEval()
 }
--- useful_things/calculator.go
+++ useful_things/calculator.go
@@ -1,15 +1,31 @@
 package useful_things
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
+func RunEval() {
+	var eq string
+
+	fmt.Println("input equation:")
+	reader := bufio.NewReader(os.Stdin)
+	eq, err := reader.ReadString('\n')
+
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("Result: %v\n", Eval(eq))
+}
+
 func Eval(equation string) int {
 	fmt.Printf("Evaluating equation: %s\n", equation)
 
-	equation = strings.Trim(equation, "\n")
+	equation = strings.Trim(equation, "\n") // Like strip in python
 	data := strings.Split(equation, " ")
 
 	firstNum, _ := strconv.Atoi(data[0])
