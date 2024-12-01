14d66f5593c81f357cef0fc29fc94e582ec3bff3
bloodofspring <t.shagin@gmail.com>
2024-12-01 20:14:14 +0300

    feat: НЕ ПИШИТЕ КОД ТАК

--- DO_NOT_WRITE_CODE_LIKE_THIS.go
+++ DO_NOT_WRITE_CODE_LIKE_THIS.go
@@ -11,32 +11,23 @@ type Position struct {
 	price      float64
 }
 
-func (p Position) checkPrice() bool {
-	return float64(p.n)*p.price == p.checkTotal
-}
-
+func (p Position) checkPrice() bool { return float64(p.n)*p.price == p.checkTotal }
 func (p Position) repr() string {
 	return fmt.Sprintf("Position{number: %d, price: %.2f, diff: %.2f}", p.n, p.price, p.checkTotal-float64(p.n)*p.price)
 }
 
-type PositionSet struct {
-	positions []Position
-}
+type PositionSet struct{ positions []Position }
 
 func (ps PositionSet) total() float64 {
 	var result float64
-
 	for _, p := range ps.positions {
 		result += p.price * float64(p.n)
 	}
-
 	return result
 }
 
 // Len - Реализация сортировка в структуре PositionSet
-func (ps PositionSet) Len() int {
-	return len(ps.positions)
-}
+func (ps PositionSet) Len() int { return len(ps.positions) }
 
 // Swap - Реализация сортировка в структуре PositionSet
 func (ps PositionSet) Swap(i, j int) {
@@ -44,65 +35,48 @@ func (ps PositionSet) Swap(i, j int) {
 }
 
 // Less - Реализация сортировка в структуре PositionSet
-func (ps PositionSet) Less(i, j int) bool {
-	return ps.positions[i].price > ps.positions[j].price
-}
-
+func (ps PositionSet) Less(i, j int) bool { return ps.positions[i].price > ps.positions[j].price }
 func ReadInput() (float64, []Position) {
 	var numberOfProducts int
 	var totalPrice float64
-
 	fmt.Print("Enter total price: ")
 	_, err := fmt.Scanf("%f", &totalPrice)
 	fmt.Print("Enter number of products: ")
 	_, err2 := fmt.Scanf("%d", &numberOfProducts)
-
 	if err != nil || err2 != nil {
 		return 0, []Position{}
 	}
-
 	var price, n, total int
 	var products []Position
 	fmt.Println("Enter data:")
-
 	for i := 0; i < numberOfProducts; i++ {
 		_, err = fmt.Scanf("%d *%d =%d", &price, &n, &total)
 		if err != nil {
 			panic(err)
 		}
-
 		products = append(products, Position{n: n, checkTotal: float64(total), price: float64(price)})
 	}
-
 	return totalPrice, products
 }
-
 func checkPositions(checkPrice float64, products []Position) ([]Position, bool) {
 	var incorrect []Position
-
 	for _, product := range products {
 		if !product.checkPrice() {
 			incorrect = append(incorrect, product)
 		}
 	}
-
 	sort.Sort(PositionSet{incorrect})
-
 	return incorrect, PositionSet{products}.total() == checkPrice
 }
-
 func Start() {
 	incorrectProducts, mainPriceCoincide := checkPositions(ReadInput())
-
 	if !mainPriceCoincide {
 		fmt.Println("Итоговая цена и список продуктов не совпадают!")
 	}
-
 	if len(incorrectProducts) == 0 {
 		fmt.Println("No incorrect products!")
 		return
 	}
-
 	fmt.Println("Incorrect products:")
 	for _, incorrectProduct := range incorrectProducts {
 		fmt.Println(incorrectProduct.repr())
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "main/ya_lyceum_1_year"
-
 func main() {
-	ya_lyceum_1_year.Start()
+	//Start()
 }
