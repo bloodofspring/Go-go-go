8553b4d65299310bad0757af0e2a508471d70313
bloodofspring <t.shagin@gmail.com>
2024-12-29 16:47:47 +0300

    feat: text commit x2

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	PointInATriangle()
+	var name string
+	fmt.Println("Enter your name:")
+	fmt.Scanf("My name is %s", &name)
+
+	fmt.Printf("Hello, %s", name)
 }
